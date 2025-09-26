de8d7a137eabcb1feb8e0789875926f38b0c7161
AlphaDrft <andreivillapaz@gmail.com>
2025-09-26 17:47:46 +0800

    Youtube Video Program Final

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -3,87 +3,118 @@ using System.Collections.Generic;
 
 class Comment
 {
-    public string Name { get; set; }
-    public string Text { get; set; }
+    private string commenterName;
+    private string text;
 
     public Comment(string name, string text)
     {
-        Name = name;
-        Text = text;
+        this.commenterName = name;
+        this.text = text;
     }
 
-    public void Display()
+    public string GetName()
     {
-        Console.WriteLine($"{Name}: {Text}");
+        return commenterName;
+    }
+
+    public string GetText()
+    {
+        return text;
     }
 }
 
 class Video
 {
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int Length { get; set; }
+    private string title;
+    private string author;
+    private int lengthInSeconds;
+    private List<Comment> comments = new List<Comment>();
 
-    private List<Comment> Comments { get; set; } = new List<Comment>();
-
-    public Video(string title, string author, int length)
+    public Video(string title, string author, int lengthInSeconds)
     {
-        Title = title;
-        Author = author;
-        Length = length;
+        this.title = title;
+        this.author = author;
+        this.lengthInSeconds = lengthInSeconds;
     }
 
     public void AddComment(Comment comment)
     {
-        Comments.Add(comment);
+        comments.Add(comment);
     }
 
     public int GetNumberOfComments()
     {
-        return Comments.Count;
+        return comments.Count;
     }
 
-    public void Display()
+    public List<Comment> GetComments()
     {
-        Console.WriteLine($"Title: {Title}");
-        Console.WriteLine($"Author: {Author}");
-        Console.WriteLine($"Length: {Length} seconds");
+        return comments;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Author: {author}");
+        Console.WriteLine($"Length: {lengthInSeconds} seconds");
         Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
         Console.WriteLine("Comments:");
 
-        foreach (var comment in Comments)
+        foreach (var comment in comments)
         {
-            comment.Display();
+            Console.WriteLine($"- {comment.GetName()}: {comment.GetText()}");
         }
 
         Console.WriteLine();
     }
 }
 
+class VideoManager
+{
+    private List<Video> videos = new List<Video>();
+
+    public void AddVideo(Video video)
+    {
+        videos.Add(video);
+    }
+
+    public List<Video> GetAllVideos()
+    {
+        return videos;
+    }
+
+    public void DisplayAllVideos()
+    {
+        foreach (var video in videos)
+        {
+            video.DisplayInfo();
+        }
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
+        VideoManager manager = new VideoManager();
+
         Video video1 = new Video("Learning C#", "Alice", 600);
         video1.AddComment(new Comment("Bob", "Great tutorial!"));
         video1.AddComment(new Comment("Charlie", "Very helpful, thanks!"));
         video1.AddComment(new Comment("Diana", "I learned a lot!"));
+        manager.AddVideo(video1);
 
         Video video2 = new Video("OOP Basics", "John", 720);
         video2.AddComment(new Comment("Eve", "Clear and concise."));
         video2.AddComment(new Comment("Frank", "Can you make more videos like this?"));
+        manager.AddVideo(video2);
 
         Video video3 = new Video("Design Patterns Intro", "Sarah", 900);
         video3.AddComment(new Comment("Grace", "This was amazing!"));
         video3.AddComment(new Comment("Heidi", "Excited for the next one."));
         video3.AddComment(new Comment("Ivan", "Very informative."));
         video3.AddComment(new Comment("Judy", "Thanks for sharing!"));
+        manager.AddVideo(video3);
 
-        List<Video> videos = new List<Video> { video1, video2, video3 };
-
-        foreach (var video in videos)
-        {
-            video.Display();
-        }
+        manager.DisplayAllVideos();
     }
 }
